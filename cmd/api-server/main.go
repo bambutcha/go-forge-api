09696d3df59f9975aac6603cd19edd033fe81ad0
bambutcha <stringdeity@gmail.com>
2025-01-04 23:09:29 +0300

    feat: create HTTP server - Added: 	configs support, 	logging, 	one route, 	tests
    [#master]

---
+++ cmd/api-server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	apiserver "github.com/bambutcha/http-rest-api/internal/app/api-server"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/api-server.toml", "config file path")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
