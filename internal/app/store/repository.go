95ebc48ce88b3939a3c59d4c8b6951ff0639348f
bambutcha <stringdeity@gmail.com>
2025-02-13 23:53:54 +0300

    feat, test: Add four middleware components, Add tests - Added 4 middleware components for my API: 	1. Verifying user authentication 	2. CORS headers 	3. RequestID 	4. Logging request - Added tests covering components
    [#master]

--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -5,5 +5,7 @@ import "github.com/bambutcha/http-rest-api/internal/app/model"
 // UserRepository ...
 type UserRepository interface {
 	Create(*model.User) error
+	FindByID(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 }
+
